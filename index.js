20c0a36c2a7de245a1a9addd24200e9f4a3a12ad
nazmuzzamanriad <86626056+nazmuzzamanriad@users.noreply.github.com>
2021-11-01 18:53:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n2jo3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n2jo3.mongodb.net/myFirstDatabase?retryWrites=true&w`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
